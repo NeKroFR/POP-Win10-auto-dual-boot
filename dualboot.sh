f6edaf24ac2e1bd7fc791d338fe58e8b467704e0
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2021-06-15 15:31:50 +0200

    make choices

--- dualboot.sh
+++ dualboot.sh
@@ -1,3 +1,22 @@
+#!/bin/sh
+
+red=`echo -en "\e[31m"`
+cyan=`echo -en "\e[36m"`
+default=`echo -en "\e[39m"`
+
+
+clear
+
+if [ "$(whoami)" != "root" ]; then
+        echo  "${red}Sorry. This script requires sudo privleges"
+        exit 255
+fi
+
+echo "${cyan}Do you want to create the dual boot?\n[Y/N]${default"
+
+read choice
+case "$choice" in
+    "y" | "Y")
 sudo apt update && sudo apt upgrade -y
 sudo apt install grub-efi grub2-common grub-customizer
 sudo grub-install
@@ -5,3 +24,10 @@ sudo cp /boot/grub/x86_64-efi/grub.efi /boot/efi/EFI/pop/grubx64.efi
 git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
 cd Top-5-Bootloader-Themes
 sudo ./install.sh
+        ;;
+    "n" | "N")
+        exit ;;
+    *)
+        echo "${red}Something unexpected happened"
+        exit 255
+esac
